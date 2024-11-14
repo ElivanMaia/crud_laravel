b7c9da57f75889f4db1ec27cfa59852a1f814019
ElivanMaia <elivan.azevedo@aluno.ce.gov.br>
2024-11-14 06:09:57 -0300

    BD

--- BD/crud_laravel.sql
+++ BD/crud_laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11/11/2024 às 05:46
+-- Tempo de geração: 14/11/2024 às 10:09
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -36,16 +36,16 @@ CREATE TABLE `agendamentos` (
   `referencias` varchar(50) DEFAULT NULL,
   `id_servico` int(10) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `id_funcionario` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Despejando dados para a tabela `agendamentos`
 --
 
-INSERT INTO `agendamentos` (`id`, `id_user`, `telefone_cliente`, `horario_agendamento`, `observacoes`, `referencias`, `id_servico`, `created_at`, `updated_at`) VALUES
-(1, 1, '(99) 9999-9997', '2024-11-06 04:59:00', 'd', 'd', 1, NULL, '2024-11-09 03:29:14'),
-(6, 6, '(99) 9999-9567', '2006-05-06 05:56:00', 'g', 'l', 11, '2024-11-09 06:05:04', '2024-11-09 06:05:04');
+INSERT INTO `agendamentos` (`id`, `id_user`, `telefone_cliente`, `horario_agendamento`, `observacoes`, `referencias`, `id_servico`, `created_at`, `updated_at`, `id_funcionario`) VALUES
+(25, 8, '(99) 9999-9567', '2024-11-15 11:00:00', 'yy', 'yy', 6, '2024-11-13 06:54:46', '2024-11-13 06:54:46', 15);
 
 -- --------------------------------------------------------
 
@@ -86,6 +86,44 @@ CREATE TABLE `failed_jobs` (
   `exception` longtext NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `feedbacks`
+--
+
+CREATE TABLE `feedbacks` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `avaliacao` int(11) NOT NULL,
+  `mensagem` text NOT NULL,
+  `sugestoes` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `funcionarios`
+--
+
+CREATE TABLE `funcionarios` (
+  `id` int(20) UNSIGNED NOT NULL,
+  `nome` varchar(125) NOT NULL,
+  `caminho_barbearia` text NOT NULL,
+  `foto` varchar(125) DEFAULT NULL,
+  `frase_pessoal` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `funcionarios`
+--
+
+INSERT INTO `funcionarios` (`id`, `nome`, `caminho_barbearia`, `foto`, `frase_pessoal`, `created_at`, `updated_at`) VALUES
+(15, 'jj', 'jj', NULL, 'jj', '2024-11-13 06:54:28', '2024-11-13 06:54:28');
 
 -- --------------------------------------------------------
 
@@ -144,7 +182,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '0001_01_01_000002_create_jobs_table', 1),
 (4, '2024_11_07_032105_add_timestamps_to_agendamentos_table', 2),
 (5, '2024_11_11_013856_add_timestamps_to_servicos_table', 3),
-(6, '2024_11_11_015444_create_servicos_table', 4);
+(6, '2024_11_11_015444_create_servicos_table', 4),
+(7, '2024_11_12_214609_add_funcionario_table', 5),
+(8, '2024_11_13_023608_create_feedbacks_table', 6);
 
 -- --------------------------------------------------------
 
@@ -165,7 +205,7 @@ CREATE TABLE `password_reset_tokens` (
 --
 
 CREATE TABLE `servicos` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+  `id` int(20) UNSIGNED NOT NULL,
   `nome_servico` varchar(125) NOT NULL,
   `preco` decimal(8,2) NOT NULL,
   `descricao` text DEFAULT NULL,
@@ -179,8 +219,7 @@ CREATE TABLE `servicos` (
 --
 
 INSERT INTO `servicos` (`id`, `nome_servico`, `preco`, `descricao`, `imagem`, `created_at`, `updated_at`) VALUES
-(3, 'Corte de Cabelo Masculino', 30.00, 'Um corte de cabelo clássico ou moderno, feito com precisão para manter o estilo masculino com um visual impecável.', 'servicos/673186124bdfb.jpg', NULL, NULL),
-(4, 'Corte de Cabelo + Barba', 40.00, 'Combinação de corte de cabelo e design de barba, oferecendo um look completo, moderno e bem cuidado.', 'servicos/67318aeed597c.jpg', NULL, NULL);
+(6, 'Corte de Cabelo Masculino', 30.00, 'jj', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -202,7 +241,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('bb9ptfHllYTAMQva2nwdLYgTwAf13EPN6EFTtfOS', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiSmk4SzJudEJZZEtwdHVScjBSVE1MVUszRWlPbnZLT0ltd2pQYzZoOSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9zZXJ2aWNvcyI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1731300185);
+('Gcg7CQVZCTacbm3gb1M9WwoVehyn37julXfg3s92', 8, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiZHNsN080M0FkenhSdmc4cjJvQllmNEVnTXRnRkhVV0tySE9GWnhhZSI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aTo4O30=', 1731471051);
 
 -- --------------------------------------------------------
 
@@ -231,7 +270,9 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `re
 (2, 'Admin2', 'admin2@gmail.com', NULL, '$2y$12$t60pj1yj9feDNBW5Qu57D.2KrwgVf7.TaxLwWC5hxXlYWwvXfhJui', NULL, '2024-11-04 07:19:36', '2024-11-04 07:19:36', 1),
 (3, 'Admin3', 'admin3@gmail.com', NULL, '$2y$12$5MGA0JeZJ66mByWRafgA8ewt/jno8plG0WtcD.Ng1Rwv7qHFN.DHi', NULL, '2024-11-04 07:20:03', '2024-11-04 07:20:03', 1),
 (4, 'Elivan', 'elivan123@gmail.com', NULL, '$2y$12$5n24azW5hV/3m0.oGyLXC.mXQlE15MD69NsXqW/zBbufG9JcCLzq2', NULL, '2024-11-07 05:17:03', '2024-11-07 05:17:03', 0),
-(6, 'maia', 'maia123@gmail.com', NULL, '$2y$12$QPeVsV4OR7Qna3hVg45T7ea2zGqEpn4DoMWtN7s6I6VKJBA0dAcpu', NULL, '2024-11-09 04:39:42', '2024-11-09 04:39:42', 0);
+(6, 'maia', 'maia123@gmail.com', NULL, '$2y$12$QPeVsV4OR7Qna3hVg45T7ea2zGqEpn4DoMWtN7s6I6VKJBA0dAcpu', NULL, '2024-11-09 04:39:42', '2024-11-09 04:39:42', 0),
+(7, 'Elivan', 'fgggggggggg@ffff.fff', NULL, '$2y$12$TFnI8R7/auKzVfjdAduUz.oC1HBnTXKH2WXvQ4WC5XR7fHVbGpvh2', NULL, '2024-11-13 00:26:32', '2024-11-13 00:26:32', 0),
+(8, 'Elivan', 'elivan1234@gmail.com', NULL, '$2y$12$02SjOS2n8jYN5QelrNspV.o9scrBao./OsfDPpCBeHd81M8/X1WDW', NULL, '2024-11-13 01:46:00', '2024-11-13 01:46:00', 0);
 
 --
 -- Índices para tabelas despejadas
@@ -242,8 +283,9 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `re
 --
 ALTER TABLE `agendamentos`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_servico` (`id_servico`),
-  ADD KEY `fk_user` (`id_user`);
+  ADD KEY `fk_user` (`id_user`),
+  ADD KEY `id_servico` (`id_servico`),
+  ADD KEY `fk_funcionario` (`id_funcionario`);
 
 --
 -- Índices de tabela `cache`
@@ -263,6 +305,18 @@ ALTER TABLE `cache_locks`
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Índices de tabela `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `jobs`
@@ -318,13 +372,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `agendamentos`
 --
 ALTER TABLE `agendamentos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de tabela `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  MODIFY `id` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de tabela `jobs`
@@ -336,19 +402,19 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT de tabela `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `servicos`
 --
 ALTER TABLE `servicos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restrições para tabelas despejadas
@@ -358,6 +424,8 @@ ALTER TABLE `users`
 -- Restrições para tabelas `agendamentos`
 --
 ALTER TABLE `agendamentos`
+  ADD CONSTRAINT `agendamentos_ibfk_1` FOREIGN KEY (`id_servico`) REFERENCES `servicos` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_funcionario` FOREIGN KEY (`id_funcionario`) REFERENCES `funcionarios` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 COMMIT;
 
